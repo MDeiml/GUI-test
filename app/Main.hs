8c48698a8bf94d09f726db4561b932cfa771a81d
mdeiml <matthias@deiml.net>
2017-11-27 17:43:37 +0100

    Updated test examples

--- app/Main.hs
+++ app/Main.hs
@@ -5,19 +5,18 @@ import Control.Arrow (returnA)
 import GLFWRenderer
 import Lib
 
--- test1 :: Widget Event [Drawable] LayoutParam () ()
--- test1 = proc _ -> do
---     widgetOutput -< (\b -> LI { layout[Drawable]s = [DrawShape (Color 255 0 0) $ Rect b], layoutParam = stdParams { pHeight = 150, pWidth = 100 }})
---     widgetOutput -< (\b -> LI { layout[Drawable]s = [DrawShape (Color 0 255 0) $ Rect b], layoutParam = stdParams { pWeightY = Just 1, pWeightX = Just 1 }})
---     widgetOutput -< (\b -> LI { layout[Drawable]s = [DrawShape (Color 0 0 255) $ Rect b], layoutParam = stdParams { pWeightY = Just 2, pWeightX = Just 1 }})
---     widgetOutput -< (\b -> LI { layout[Drawable]s = [DrawShape (Color 255 255 255) $ Rect b], layoutParam = stdParams { pHeight = 50, pWidth = 40 }})
-    -- widgetOutput -< (\b -> LI { layout[Drawable]s = [DrawShape (Color 255 255 255) $ Text "Test" (Coords 0 0) 20], layoutParam = stdParams { pHeight = 50, pWidth = 40 }})
+test1 :: Widget Event [Drawable] LayoutParam () ()
+test1 = proc _ -> do
+    widgetOutput stdParams { pHeight = 150, pWidth = 100 } -< (\b -> [DrawShape (Color 255 0 0) $ Rect b])
+    widgetOutput stdParams { pWeightY = Just 1, pWeightX = Just 1 } -< (\b -> [DrawShape (Color 0 255 0) $ Rect b])
+    widgetOutput stdParams { pWeightY = Just 2, pWeightX = Just 1 } -< (\b -> [DrawShape (Color 0 0 255) $ Rect b])
+    widgetOutput stdParams { pHeight = 50, pWidth = 40 } -< (\b -> [DrawShape (Color 255 255 255) $ Rect b])
 
--- test2 :: Widget Event [Drawable] LayoutParam () ()
--- test2 = widgetLayout (tableLayout 2 (Nothing, Just 1)) test1
+test2 :: Widget Event [Drawable] LayoutParam () ()
+test2 = widgetLayout (tableLayout 2 (Nothing, Just 1)) test1
 
--- test2' :: Widget Event [Drawable] LayoutParam () ()
--- test2' = widgetLayout (linearLayout Vertical (Nothing, Just 1)) test1
+test2' :: Widget Event [Drawable] LayoutParam () ()
+test2' = widgetLayout (linearLayout Vertical (Nothing, Just 1)) test1
 
 focusWidget :: Widget Event [Drawable] LayoutParam () Bool
 focusWidget = widgetLayout (stackLayout (0,0,0,0) (AlignCenter, AlignCenter) (Just 1, Just 1)) $ proc _ -> do
@@ -38,4 +37,4 @@ test3 = widgetLayout (tableLayout 2 (Just 1, Just 1)) $ proc _ -> do
 main :: IO ()
 main = do
     r <- create "Test" (800,600) :: IO GLFWRenderer
-    mainLoop r test3
+    mainLoop r test2'
